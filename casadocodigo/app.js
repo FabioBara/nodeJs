b5a7833402f66f9be7f6ed309263d58da81cdb28
FabioBara <fabio.bara@icloud.com>
2017-10-11 01:06:53 -0300

    NA

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -1,12 +1,8 @@
-var express = require('express');
-var app = express();
+var app = require('./config/express')();
 
-app.set('view engine','ejs');
+var rotasProdutos = require('./app/routes/produtos');
 
-
-app.get("/produtos", function(req, res){
-    res.render("produtos/lista");
-});
+rotasProdutos(app);
 
 app.listen(3000, function(){
     console.log("servidor rodando teste!");
