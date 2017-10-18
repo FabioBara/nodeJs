525f2b7a947e8dbc2041970509379a9dfb65de94
FabioBara <fabio.bara@icloud.com>
2017-10-18 02:00:40 -0200

    conexao MySql

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,6 +1,22 @@
 module.exports = function(app){
     app.get("/produtos", function(req, res){
-        res.render("produtos/lista"); 
+
+        var mySQL = require('mysql');
+        var con = mySQL.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : '1234',
+            database: 'casadocodigo'
+        });
+
+        con.query('SELECT * FROM casadocodigo.livros', function(err, result){
+            res.render("produtos/lista", {lista:result});
+           // res.send(result, {lista:result});
+        });
+
+
+        con.end();
+        //res.render("produtos/lista"); 
     });
 }
 
